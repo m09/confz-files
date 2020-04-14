6667ea3ed4672348f475be17ebd59dc413cbae4e
m09 <142691+m09@users.noreply.github.com>
2020-04-14 14:50:42 +0200

    Fix symbolic links in find & remove tmux
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- zsh/zsh-custom/aliases.zsh
+++ zsh/zsh-custom/aliases.zsh
@@ -1,4 +1,4 @@
-files="$(find "$ZSH_CUSTOM/alias/" -type f -name '*.zsh-alias' -print0 | sort -z)"
+files="$(find -L "$ZSH_CUSTOM/alias/" -type f -name '*.zsh-alias' -print0 | sort -z)"
 IFS=$'\0' read -r -A files <<< "$files"
 for f in "${files[@]}"; do
     if [[ -f "$f" ]]; then source "$f"; fi
