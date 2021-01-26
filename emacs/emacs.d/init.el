ee5de64992a8402fd655dacfded701633d828001
m09 <142691+m09@users.noreply.github.com>
2021-01-26 10:10:50 +0100

    Add magit to emacs plugins
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -71,7 +71,7 @@ Do it recursively if the third argument is not nil."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode multiple-cursors undo-tree))))
+ '(package-selected-packages (quote (magit markdown-mode multiple-cursors undo-tree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
