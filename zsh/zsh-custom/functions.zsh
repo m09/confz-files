27c0f09a4c965ec0c282493a1e85932fbb6022f0
m09 <142691+m09@users.noreply.github.com>
2023-03-14 09:49:31 +0800

    Add a function to increase sound limit on debian
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- zsh/zsh-custom/functions.zsh
+++ zsh/zsh-custom/functions.zsh
@@ -9,6 +9,16 @@ function aziz() {
     redshift-gtk -l 47.2173:-1.5534 -r -b "$brightness":"$brightness" & disown
 }
 
+function son() {
+    local sound
+    if [[ $# > 0 ]]; then
+        sound="$1%"
+    else
+        sound="100%"
+    fi
+    pactl -- set-sink-volume 0 "$sound"
+}
+
 function optimize-png() {
     local img="$1"
     pngquant -f "$img" -o "$img"
